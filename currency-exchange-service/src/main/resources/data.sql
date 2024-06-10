bc3d59a6127c06e89bb02f421fe76ed94d3a0eb1
Mohsin Raza <mohsin.raza@10pearls.com>
2024-06-10 20:19:25 +0500

    Currency Exchange JPA Setup

--- currency-exchange-service/src/main/resources/data.sql
+++ currency-exchange-service/src/main/resources/data.sql
@@ -1,11 +1,11 @@
 insert into currency_exchange
 (id,currency_from,currency_to,conversion_multiple,environment)
-values(10001,'USD','INR',65,'');
+values(10001,'USD','PKR',278,'');
 
 insert into currency_exchange
 (id,currency_from,currency_to,conversion_multiple,environment)
-values(10002,'EUR','INR',75,'');
+values(10002,'EUR','PKR',298.54,'');
 
 insert into currency_exchange
 (id,currency_from,currency_to,conversion_multiple,environment)
-values(10003,'AUD','INR',25,'');
+values(10003,'AUD','PKR',183.50,'');
