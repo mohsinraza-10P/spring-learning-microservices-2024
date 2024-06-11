d316eb1fc3ee257c2f596e5b2b762dc9abef90cc
Mohsin Raza <mohsin.raza@10pearls.com>
2024-06-11 13:34:27 +0500

    Used FeignClient for service invokation

--- currency-conversion-service/src/main/java/com/mohsin/microservices/currency_conversion_service/controllers/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/mohsin/microservices/currency_conversion_service/controllers/CurrencyConversionController.java
@@ -1,6 +1,7 @@
 package com.mohsin.microservices.currency_conversion_service.controllers;
 
 import com.mohsin.microservices.currency_conversion_service.bean.CurrencyConversion;
+import com.mohsin.microservices.currency_conversion_service.proxy.CurrencyExchangeProxy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,18 +14,20 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 
 @RestController
-@RequestMapping("currency-conversion")
 public class CurrencyConversionController {
 
     @Autowired
     private Environment environment;
 
+    @Autowired
+    private CurrencyExchangeProxy proxy;
+
     // http://localhost:8100/currency-conversion/from/USD/to/PKR/quantity/10
     // To run more instances of this app on other ports, create new configuration in Intellij and add following to VM option
     // -Dserver.port=81001
 
-    @GetMapping("/from/{fromCurrency}/to/{toCurrency}/quantity/{quantity}")
-    public CurrencyConversion getExchangeValue(
+    @GetMapping("/currency-conversion/from/{fromCurrency}/to/{toCurrency}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversion(
             @PathVariable String fromCurrency,
             @PathVariable String toCurrency,
             @PathVariable BigDecimal quantity
@@ -46,6 +49,23 @@ public class CurrencyConversionController {
         }
         currencyConversion.setQuantity(quantity);
         currencyConversion.setTotalCalculatedAmount(currencyConversion.getConversionMultiple().multiply(quantity));
+        currencyConversion.setEnvironment(currencyConversion.getEnvironment() + " - RestTemplate");
+        return currencyConversion;
+    }
+
+    @GetMapping("/currency-conversion-feign/from/{fromCurrency}/to/{toCurrency}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversionViaFeign(
+            @PathVariable String fromCurrency,
+            @PathVariable String toCurrency,
+            @PathVariable BigDecimal quantity
+    ) {
+        var currencyConversion = proxy.getExchangeValue(fromCurrency, toCurrency);
+        if (currencyConversion == null) {
+            throw new RuntimeException("Unable to find data for " + fromCurrency + " to " + toCurrency);
+        }
+        currencyConversion.setQuantity(quantity);
+        currencyConversion.setTotalCalculatedAmount(currencyConversion.getConversionMultiple().multiply(quantity));
+        currencyConversion.setEnvironment(currencyConversion.getEnvironment() + " - FeignClient/Proxy");
         return currencyConversion;
     }
 }
---
+++ currency-conversion-service/src/main/java/com/mohsin/microservices/currency_conversion_service/proxy/CurrencyExchangeProxy.java
@@ -0,0 +1,17 @@
+package com.mohsin.microservices.currency_conversion_service.proxy;
+
+import com.mohsin.microservices.currency_conversion_service.bean.CurrencyConversion;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+// Name: Typically the service name we want to call
+@FeignClient(name = "currency-exchange", url = "localhost:8000")
+public interface CurrencyExchangeProxy {
+
+    @GetMapping("/currency-exchange/from/{fromCurrency}/to/{toCurrency}")
+    public CurrencyConversion getExchangeValue(
+            @PathVariable String fromCurrency,
+            @PathVariable String toCurrency
+    );
+}
